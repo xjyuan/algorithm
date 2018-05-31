f4bd53edfb2b78bf507855e78f0af3c4e84c3000
chenying <chenying@lyancoffee.com>
2018-05-31 16:03:57 +0800

    leetcode：三数之和

--- src/main/java/com/cy/leetcode/MyAtoi_8.java
+++ src/main/java/com/cy/leetcode/MyAtoi_8.java
@@ -55,51 +55,53 @@ public class MyAtoi_8 {
         int result = new Solution().myAtoi(str);
         Assert.isTrue(result == Integer.MIN_VALUE);
     }
-}
 
-class Solution {
-    public int myAtoi(String str) {
-        if (str == null || str.length() == 0) {
-            return 0;
-        }
-        int index = 0;
-        while ((str.charAt(index)) == ' ') {
-            index++;
-            if (index >= str.length()) {
-                return 0;
-            }
-        }
-        boolean isNegative = false;
-        if (str.charAt(index) == '-') {
-            index++;
-            if (index >= str.length()) {
-                return 0;
-            }
-            isNegative = true;
-        } else if (str.charAt(index) == '+') {
-            index++;
-            if (index >= str.length()) {
-                return 0;
-            }
-        }
-        char firstNum = str.charAt(index);
-        if (!Character.isDigit(firstNum)) {
-            return 0;
-        }
 
-        int result = 0;
-        while (index < str.length() && Character.isDigit(str.charAt(index))) {
-            int thisNum = Character.getNumericValue(str.charAt(index));
-            if (isNegative) {
-                thisNum = -thisNum;
+    class Solution {
+        public int myAtoi(String str) {
+            if (str == null || str.length() == 0) {
+                return 0;
             }
-            int temp = result;
-            result = result * 10 + thisNum;
-            if (temp != 0 && Math.abs(temp) > Math.abs(Integer.MIN_VALUE) / 10 && !(result + "").startsWith(temp + "")){
-                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            int index = 0;
+            while ((str.charAt(index)) == ' ') {
+                index++;
+                if (index >= str.length()) {
+                    return 0;
+                }
             }
-            index++;
+            boolean isNegative = false;
+            if (str.charAt(index) == '-') {
+                index++;
+                if (index >= str.length()) {
+                    return 0;
+                }
+                isNegative = true;
+            } else if (str.charAt(index) == '+') {
+                index++;
+                if (index >= str.length()) {
+                    return 0;
+                }
+            }
+            char firstNum = str.charAt(index);
+            if (!Character.isDigit(firstNum)) {
+                return 0;
+            }
+
+            int result = 0;
+            while (index < str.length() && Character.isDigit(str.charAt(index))) {
+                int thisNum = Character.getNumericValue(str.charAt(index));
+                if (isNegative) {
+                    thisNum = -thisNum;
+                }
+                int temp = result;
+                result = result * 10 + thisNum;
+                if (temp != 0 && Math.abs(temp) > Math.abs(Integer.MIN_VALUE) / 10 && !(result + "").startsWith(temp + "")) {
+                    return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+                index++;
+            }
+            return result;
         }
-        return result;
     }
+
 }
