2704868a1bf70a80e08d26eeef431bd12300b287
ying.chen2 <ying.chen2@nio.com>
2018-07-20 09:09:20 +0800

    由图片生成矩阵文件

---
+++ src/test/java/com/cy/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.cy.demo;
+
+import java.util.Random;
+
+/**
+ * @Author chenying
+ * @Date 2018/7/19
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        Random random = new Random();
+        char x= (char)65;
+        System.out.println(x);
+    }
+}
